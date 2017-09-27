4aa4cf1aabf8b77c0e6906e6dc3a3add4f3df859
Jozef Randjak <randjakjozef@gmail.com>
2017-09-27 11:43:49 +0200

    Process transport classes into one event

---
+++ TruckLogistic/TruckLogistic/ProcessTransport.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace TruckLogistic
+{
+    public class ProcessTransport : Transport
+    {
+        public List<ProcessTransport> ProcessEvents(List<CargoTransport> cargosTransport, List<TruckTransport> truckTransport)
+        {
+            List<ProcessTransport> events = new List<ProcessTransport>();
+
+            foreach (var truck in truckTransport)
+            {
+                ProcessTransport transport = new ProcessTransport();
+                transport.Capacity = truck.Capacity;
+                transport.Date = truck.Date;
+                transport.Name = truck.Name;
+                events.Add(transport);
+            }
+
+            foreach (var cargo in cargosTransport)
+            {
+                ProcessTransport transport = new ProcessTransport();
+                transport.Name = cargo.Name;
+                transport.Date = cargo.Date;
+                transport.Capacity = cargo.Capacity;
+                events.Add(transport);
+            }
+            return events;
+        }
+    }
+}
--- TruckLogistic/TruckLogistic/Program.cs
+++ TruckLogistic/TruckLogistic/Program.cs
@@ -5,31 +5,17 @@ using System.Threading.Tasks;
 
 namespace TruckLogistic
 {
-    partial class program
+    partial class Program
     {
         static void Main(string[] args)
         {
-
-            TruckTransport transport = new TruckTransport();
+            TruckTransport truckTransport = new TruckTransport();
             CargoTransport cargoTransport = new CargoTransport();
 
-            Console.WriteLine("trucks");
-
-            foreach (var truck in transport.AcceptIncomingTrucks())
-            {
-                Console.WriteLine(truck.Name);
-                Console.WriteLine(truck.Date);
-                Console.WriteLine(truck.Capacity);
-            }
-
-            Console.WriteLine("cargo");
-
-            foreach (var cargo in cargoTransport.AcceptIncomingCargo())
-            {
-                Console.WriteLine(cargo.Name);
-                Console.WriteLine(cargo.Date);
-                Console.WriteLine(cargo.Capacity);
-            }
+            ProcessTransport transport = new ProcessTransport();
+            
+            transport.ProcessEvents(cargoTransport.AcceptIncomingCargo(),truckTransport.AcceptIncomingTrucks());
+            
 
         }
     }
